86314539a0eb4f74a3b033dabbe5d96ad1ee83ef
David Pablo Cardoso Breda Queiroz <davidpablo99@gmail.com>
2025-04-25 20:40:10 -0400

    Criando componentes

--- react-funtamentals/src/App.jsx
+++ react-funtamentals/src/App.jsx
@@ -1,10 +1,13 @@
+import StatusTest from "./components/StatusTest"
+import Title from "./components/Title"
+
 export default function App(){
-  const tech = "React"
-  const status = false
+  
+
   return (
     <div>
-      <h1>{tech} is awesome</h1>
-      <h2>Current status: {status ? "ON" : "OFF"}</h2>
+      <Title/>
+      <StatusTest/>
     </div>
   )
 }
