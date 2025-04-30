d8e8235d83c70aacb02996c317078297e8f0420c
David Pablo Cardoso Breda Queiroz <davidpablo99@gmail.com>
2025-04-30 15:00:23 -0400

    Adicionando props, e aprendendo elas

--- 2-exercicio/src/App.jsx
+++ 2-exercicio/src/App.jsx
@@ -1,5 +1,14 @@
 import Card from "./components/Card";
+import swPosterImg from './assets/ba9464145eba8762f6286a3c8387c951.jpg';
+import pesbPosterImg from './assets/esb-poster.webp';
+import rotjPosterImg from './assets/rotj-poster.webp';
 
 export default function App(){
-  return <Card/>
+  return (
+    <>
+      <Card title="Pôster: Star Wars (1977)" poster={swPosterImg}/>
+      <Card title="Pôster: Empire Strikes Back (1980)" poster={pesbPosterImg}/>
+      <Card title="Pôster: Return of the Jedi (1983)" poster={rotjPosterImg}/>
+    </>
+  )
 }
---
+++ 2-exercicio/src/components/Button/index.jsx
@@ -0,0 +1,6 @@
+import styles from './styles.module.css';
+export default function Button(){
+    return (
+        <button className={styles.wrapper}>Comprar agora</button>
+    )
+}
